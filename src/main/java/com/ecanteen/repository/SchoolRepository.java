8e05619c8d5672cf04dc5a813a3eff566ce91702
hamid-mobilityonegroup <hamid@mobilityonegroup.com>
2022-07-10 17:38:09 +0430

    updating

--- src/main/java/com/ecanteen/repository/SchoolRepository.java
+++ src/main/java/com/ecanteen/repository/SchoolRepository.java
@@ -2,7 +2,6 @@ package com.ecanteen.repository;
 
 import com.ecanteen.domain.School;
 import org.springframework.data.jpa.repository.*;
-import org.springframework.stereotype.Repository;
 
 /**
  * Spring Data SQL repository for the School entity.
